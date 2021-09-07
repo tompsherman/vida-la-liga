83ea344285c1329e50b3ed6ead0f3f3965693a35
unknown <tompsherman@gmail.com>
2021-09-07 18:44:30 -0400

    initial setup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>La Liga Donk Fantastica</h1>
+      <h2>18 Red Beard Road</h2>
+    </div>
+  );
+};
+
+export default App;
